d066fca9e5b5e92f4a3d7caaea695361046fb0b8
Matthew Wood <mawoodmain@googlemail.com>
2017-11-10 17:28:15 +0000

    Fixed ids

--- src/fishing/Main.java
+++ src/fishing/Main.java
@@ -11,10 +11,10 @@ public class Main
     public static void main(String[] args)
     {
         Catch.catches.add(new Fish(0,"Carp", BowSize.LARGE_LAKE, Arrays.asList(BowType.STATIC_FRESH,BowType.FLOWING_FRESH)));
-        Catch.catches.add(new Fish(0,"Perch", BowSize.POND, Arrays.asList(BowType.STATIC_FRESH,BowType.FLOWING_FRESH)));
-        Catch.catches.add(new Fish(0,"Salmon", BowSize.RIVER, Arrays.asList(BowType.STATIC_SALTWATER,BowType.FLOWING_SALTWATER)));
-        Catch.catches.add(new Junk(1,"boot"));
-        Catch.catches.add(new Junk(1,"Damaged tackle"));
+        Catch.catches.add(new Fish(1, "Perch", BowSize.POND, Arrays.asList(BowType.STATIC_FRESH, BowType.FLOWING_FRESH)));
+        Catch.catches.add(new Fish(2, "Salmon", BowSize.RIVER, Arrays.asList(BowType.STATIC_SALTWATER, BowType.FLOWING_SALTWATER)));
+        Catch.catches.add(new Junk(3, "boot"));
+        Catch.catches.add(new Junk(4, "Damaged tackle"));
 
         BodyOfWater testBody = new BodyOfWater(1.0,BowSize.POND, BowType.STATIC_FRESH);
         System.out.println("Freshwater pond:\t\t" + testBody.getPossibleCatches());
